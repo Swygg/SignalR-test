428ffa6c6e83aefa6154280f016e56619f057188
Vincent <74617568+Swygg@users.noreply.github.com>
2021-09-04 14:31:08 +0200

    first commit

---
+++ ConsoleIHM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleIHM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
